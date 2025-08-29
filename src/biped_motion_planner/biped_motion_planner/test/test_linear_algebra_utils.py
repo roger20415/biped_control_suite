df9289b4474bcc4323b6cf749793778f6a60c607
roger20415 <roger20415@gmail.com>
2025-08-29 23:22:09 +0800

    [FEAT] add test for invert_transformation_matrix
    - test function invert_transformation_matrix in linear_algebra_utils.py

--- src/biped_motion_planner/biped_motion_planner/test/test_linear_algebra_utils.py
+++ src/biped_motion_planner/biped_motion_planner/test/test_linear_algebra_utils.py
@@ -58,3 +58,35 @@ def test_normalize_vec(linear_algebra_utils):
     vec_expected = np.array([0.6, 0.8, 0.0], dtype=np.float64)
     vec_computed = linear_algebra_utils.normalize_vec(vec)
     assert np.allclose(vec_computed, vec_expected, atol=1e-12)
+
+def test_invert_transformation_matrix(linear_algebra_utils):
+    T_1 = np.array([
+        [0.474747474747475, -0.464646464646465, -0.747474747474747, 1.0],
+        [-0.141414141414141, 0.797979797979798, -0.585858585858586, 2.0],
+        [0.868686868686869, 0.383838383838384, 0.313131313131313, 3.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ], dtype=np.float64)
+    T_expected_1 = np.array([
+        [ 0.474747474748, -0.141414141414,  0.868686868687, -2.79797979798 ],
+        [-0.464646464646,  0.797979797980,  0.383838383838, -2.28282828283 ],
+        [-0.747474747475, -0.585858585859,  0.313131313131,  0.97979797998 ],
+        [ 0.0,             0.0,             0.0,             1.0           ]
+    ], dtype=np.float64)
+    T_computed_1 = linear_algebra_utils._invert_transformation_matrix(T_1)
+
+    T_2 = np.array([
+        [ 0.353553, -0.612372,  0.707107,  1.5 ],
+        [ 0.926777,  0.126826, -0.353553, -2.0 ],
+        [ 0.126826,  0.780330,  0.612372,  0.5 ],
+        [ 0.0,       0.0,       0.0,       1.0 ]
+    ], dtype=np.float64)
+    T_expected_2 = np.array([
+        [ 0.353553,  0.926777,  0.126826,  1.259812 ],
+        [-0.612372,  0.126826,  0.780330,  0.782045 ],
+        [ 0.707107, -0.353553,  0.612372, -2.073953 ],
+        [ 0.0,       0.0,       0.0,       1.0      ]
+    ], dtype=np.float64)
+    T_computed_2 = linear_algebra_utils._invert_transformation_matrix(T_2)
+
+    assert np.allclose(T_computed_1, T_expected_1, atol=1e-12)
+    assert np.allclose(T_computed_2, T_expected_2, atol=1e-12)
