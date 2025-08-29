50ee9120f88f6605875038acbd674ab35a0653f5
roger20415 <roger20415@gmail.com>
2025-08-29 23:03:44 +0800

    [TEST] test combine_transformation_matrix
    - Add unit test for combine_transformation_matrix function in
      linear_algebra_utils.py

--- src/biped_motion_planner/biped_motion_planner/test/test_linear_algebra_utils.py
+++ src/biped_motion_planner/biped_motion_planner/test/test_linear_algebra_utils.py
@@ -1,4 +1,4 @@
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Vector3
 import numpy as np
 import pytest
 from biped_motion_planner.linear_algebra_utils import LinearAlgebraUtils
@@ -33,6 +33,22 @@ def test_quaternion_to_rotation_matrix(linear_algebra_utils):
         [0.0,  1.0,  0.0]
     ], dtype=np.float64)
     R_computed_2 = linear_algebra_utils.quaternion_to_rotation_matrix(q_2)
-    
+
     assert np.allclose(R_computed_1, R_expected_1, atol=1e-12)    
     assert np.allclose(R_computed_2, R_expected_2, atol=1e-12)
+
+def test_combine_transformation_matrix(linear_algebra_utils):
+    R = np.array([
+        [0.474747474747475, -0.464646464646465, -0.747474747474747],
+        [-0.141414141414141, 0.797979797979798, -0.585858585858586],
+        [0.868686868686869, 0.383838383838384, 0.313131313131313]
+    ], dtype=np.float64)
+    t = Vector3(x=1.0, y=2.0, z=3.0)
+    T_expected = np.array([
+        [0.474747474747475, -0.464646464646465, -0.747474747474747, 1.0],
+        [-0.141414141414141, 0.797979797979798, -0.585858585858586, 2.0],
+        [0.868686868686869, 0.383838383838384, 0.313131313131313, 3.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ], dtype=np.float64)
+    T_computed = linear_algebra_utils.combine_transformation_matrix(R, t)
+    assert np.allclose(T_computed, T_expected, atol=1e-12)
