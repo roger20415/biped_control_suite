8eb1e898991ea05dc152e3b4e1d297da8d7a6e17
roger20415 <roger20415@gmail.com>
2025-08-29 21:43:39 +0800

    [FEAT] add _calc_R_BL

--- src/biped_motion_planner/biped_motion_planner/joint_targets_calculator.py
+++ src/biped_motion_planner/biped_motion_planner/joint_targets_calculator.py
@@ -8,10 +8,13 @@ L_FOOT: float = 1.0
 
 class JointTargetsCalculator():
     def __init__(self):
-        self.p_W: dict[str, Vector3] = {}
-        self.p_B: dict[str, Vector3] = {}
-        self.p_L: dict[str, Vector3] = {}
-        self.p_uw: dict[str, tuple[float, float]] = {}
+        self.p_W: dict[str, Vector3] = {} # points in the World frame
+        self.p_B: dict[str, Vector3] = {} # points in the Baselink frame
+        self.p_L: dict[str, Vector3] = {} # points in the Leg frame
+        self.p_uw: dict[str, tuple[float, float]] = {} # points in the (u,w) coordinates in the Leg frame
+        self.joint_phi: dict[str, float] = {} # +u-axis rotation in the Leg frame
+        self.joint_theta: dict[str, float] = {} # -v-axis rotation in the Leg frame
+        self.joint_targets: dict[str, float] = {} # final joint angle command
 
     def calc_joint_targets(self, p_W: Mapping[str, Vector3], q_W_baselink: Quaternion) -> dict[str, float]:
         self.p_W = dict(p_W)
@@ -28,3 +31,20 @@ class JointTargetsCalculator():
         T_WB = LinearAlgebraUtils.combine_transformation_matrix(R_WB, p_W_baselink)
         T_BW = LinearAlgebraUtils._invert_transformation_matrix(T_WB)
         return T_BW
+    
+    def _calc_R_BL(self) -> NDArray[np.float64]:
+        """
+        The Leg frame must follow the following three rules:
+        1. The origin is located at the hip joint.
+        2. The u-axis is parallel to the x-axis of the Baselink frame.
+        3. The uw-plane passes through the foot point.
+        """
+        foot_to_hip_vec = Vector3(self.p_B["hip"].x - self.p_B["foot"].x,
+                        self.p_B["hip"].y - self.p_B["foot"].y,
+                        self.p_B["hip"].z - self.p_B["foot"].z)
+        norm_w = np.linalg.norm(np.array([foot_to_hip_vec.y, foot_to_hip_vec.z])) # length of the original w vector 
+        u = np.array([1.0, 0.0, 0.0])
+        w = np.array([0.0, foot_to_hip_vec.y/norm_w, foot_to_hip_vec.z/norm_w])
+        v = -np.cross(u, w)
+
+        return np.column_stack((u, v, w))
