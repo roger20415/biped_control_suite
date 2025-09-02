80c1d48f825a293a94d86b218dc9c1fcc7a7cfb6
roger20415 <roger20415@gmail.com>
2025-09-02 10:49:26 +0800

    [FIX] typing error fix (NDArray)
    - NDArray[np.float64, np.float64] is wrong typing
    - _calc_p_uw_ankle returns NDArray[np.float64], not float

--- src/biped_motion_planner/biped_motion_planner/joint_targets_calculator.py
+++ src/biped_motion_planner/biped_motion_planner/joint_targets_calculator.py
@@ -11,7 +11,7 @@ class JointTargetsCalculator():
         self.p_W: dict[str, Vector3] = {} # points in the World frame
         self.p_B: dict[str, Vector3] = {} # points in the Baselink frame
         self.p_L: dict[str, Vector3] = {} # points in the Leg frame
-        self.p_uw: dict[str, NDArray[np.float64, np.float64]] = {} # points in the (u,w) coordinates in the Leg frame
+        self.p_uw: dict[str, NDArray[np.float64]] = {} # points in the (u,w) coordinates in the Leg frame
         self.joint_phi: dict[str, float] = {} # +u-axis rotation in the Leg frame
         self.joint_theta: dict[str, float] = {} # -v-axis rotation in the Leg frame
         self.joint_targets: dict[str, float] = {} # final joint angle commands
@@ -28,6 +28,7 @@ class JointTargetsCalculator():
         self.p_uw.update(self._transform_points_Leg_to_uw())
         self.joint_phi["leg"] = self._calc_phi_BL()
         e_L_proj = self._project_gravity_to_uw_plane(R_WB.T, R_BL.T)
+        self.p_uw["ankle"] = self._calc_p_uw_ankle(e_L_proj)
 
     def _transform_points_World_to_Baselink(self, T_BW: NDArray[np.float64]) -> dict[str, Vector3]:
         p_B_hip = LinearAlgebraUtils.transform_point(T_BW, self.p_W["hip"])
@@ -105,7 +106,7 @@ class JointTargetsCalculator():
         e_L_proj = R_LB @ e_B_proj
         return e_L_proj
     
-    def _calc_p_uw_ankle(self, e_L_proj: NDArray[np.float64]) -> float:
+    def _calc_p_uw_ankle(self, e_L_proj: NDArray[np.float64]) -> NDArray[np.float64]:
         e_uw_proj: NDArray[np.float64] = e_L_proj[[0, 2]]
         e_uw_proj_norm = LinearAlgebraUtils.normalize_vec(e_uw_proj)
         d_uw_ankle = Config.L_ANKLE * e_uw_proj_norm
