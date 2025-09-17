a013552238cd8d4e03e865a8a68c73d9288bb7f6
roger20415 <roger20415@gmail.com>
2025-09-17 17:39:58 +0800

    [FEAT] clean biped_motion_planner_node

--- src/biped_motion_planner/biped_motion_planner/biped_motion_planner_node.py
+++ src/biped_motion_planner/biped_motion_planner/biped_motion_planner_node.py
@@ -6,92 +6,21 @@ from .config import LegSide
 
 STANCE_LEG_JOINT_ALPHA: float = 5.0 # in degrees
 VALID_LEG_SIDES: tuple[str, ...] = ("left", "right")
+VALID_SUPPORT_SIDES: tuple[str, ...] = ("left", "right", "mid")
 
-class StanceLegControlNode(Node):
+class BipedMotionPlannerNode(Node):
     def __init__(self):
-        super().__init__('stance_leg_control_node')
-        self._leg_side: LegSide = "undefined"
+        super().__init__('biped_motion_planner_node')
+        self._swing_leg_side: LegSide = "undefined"
+        self._stance_leg_side: LegSide = "undefined"
+        self._support_side: str = "undefined"
 
-        self._stance_side_subscriber_ = self.create_subscription(
-            String,
-            '/biped/stance_side',
-            self._stance_side_callback,
-            10
-        )
-        self._stance_leg_alpha_subscriber_ = self.create_subscription(
-            Float32,
-            '/biped/stance_leg_alpha',
-            self._stance_leg_alpha_callback,
-            10
-        )
 
-        self._left_joint_target_publisher_ = self.create_publisher(
-            Float64MultiArray,
-            '/biped/left_joint_target',
-            10
-        )
-        self._right_joint_target_publisher_ = self.create_publisher(
-            Float64MultiArray,
-            '/biped/right_joint_target',
-            10
-        )
-    
-    def _stance_leg_alpha_callback(self, msg: Float32) -> None:
-        if self._leg_side not in VALID_LEG_SIDES:
-            self.get_logger().warn(f"Leg side is invalid: {self._leg_side}")
-            return
-        leg_side = self._leg_side
-        joint_pose = self._compose_joint_pose_for_publish(msg.data, leg_side)
-        self._pub_joint_pos(joint_pose, leg_side)
 
-    def _stance_side_callback(self, msg: String) -> None:
-        if msg.data not in ("left", "right"):
-            self.get_logger().error(f"Invalid stance side: {msg.data}. Must be 'left' or 'right'.")
-            return
-        if msg.data != self._leg_side:
-            self.get_logger().info(f"Switching stance side from {self._leg_side} to {msg.data}.")
-            self._leg_side = msg.data
-
-    def _pub_joint_pos(self, joint_pos: list[float], leg_side: str) -> None:
-        msg = Float64MultiArray()
-        msg.data = [float(i) for i in joint_pos]
-        if leg_side == "left":
-            self._left_joint_target_publisher_.publish(msg)
-        elif leg_side == "right":
-            self._right_joint_target_publisher_.publish(msg)
-        else:
-            self.get_logger().error(f"Invalid leg side: {leg_side}. Cannot publish joint targets.")
-
-    def _compose_joint_pose_for_publish(self, leg_alpha: float, leg_side: str) -> list[float]:
-        if leg_side == "left":
-            joint_pose: list[float] = [
-                0.0, # hip
-                -leg_alpha, # thigh
-                0.0, # calf
-                -leg_alpha, # ankle
-                0.0 # foot
-            ]
-
-        elif leg_side == "right":
-            joint_pose: list[float] = [
-                0.0, # hip
-                leg_alpha, # thigh
-                0.0, # calf
-                leg_alpha, # ankle
-                0.0 # foot
-            ]
-        else:
-            self.get_logger().error(f"Invalid leg side: {leg_side}. Cannot compose joint pose.")
-            return [0.0]*JOINT_NUMS
-        
-        if len(joint_pose) != JOINT_NUMS:
-            raise ValueError("Invalid swing leg joint pose length.")
-        return joint_pose
-    
 
 def main(args=None):
     rclpy.init(args=args)
-    node = StanceLegControlNode()
+    node = BipedMotionPlannerNode()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
